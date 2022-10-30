e8f1fe5ee1aa53b8e08ffa85d8c650a319210ed3
18436325282 <104488353+18436325282@users.noreply.github.com>
2022-10-30 10:17:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+  USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wxadebc17f202b3422',
 
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'og-D451JhkndfF9gL_IwsrxKWI3s',
+      id: 'PDU17402TWBt9IBG65u76nNrHYQRcNi0BpXtxUw92',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ViwWkZ9QXtc3RrfI0_AT-n_dEuYJFhQPexKLN8lUF-8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
