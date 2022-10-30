888220ec84a9b26a4eafa03f242f1be736122b02
18436325282 <104488353+18436325282@users.noreply.github.com>
2022-10-30 08:54:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '	ViwWkZ9QXtc3RrfI0_AT-n_dEuYJFhQPexKLN8lUF-8',
 
   CALLBACK_USERS: [
     {
